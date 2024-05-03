57b38d893e8763d07dde2cdc7ec5941a7b045aa0
lance-taylor <43751972+lance-taylor@users.noreply.github.com>
2024-05-03 02:17:58 -0400

    Update making tables and figures_IN PROGRESS.R
    To use: 
    
    specify:
    1) basePath - folder you are keeping the industry data in
    2) industryFile - the industry data filename, including its type extension eg .csv
    3) nVar - the name of the variable in the data file that you want to use as count of establishments (Emily provides multiple definitions) 
    4) popVar - the name of the variable in the data that is the baseline population variable in S(Y) 
    5) yVars - the names of the other Y variables measured in population counts that you wish to use
    6) zVars - the names of demand shifting variables Z (as in Bresnahan and Reiss 1991) that you want to use
    7) wVars - the names of the demand and cost shifting variables W (as in Bresnahan and Reiss 1991) that you want to use
    8) poolOver - the highest number of establishments in a market over which everything else is pooled to. Eg, everything above n is pooled with n into n+
    
    highlight and run everything via ctrl A + ctrl enter
    
    In the basePath folder that you specified, you will now have a new folder named [INDUSTRY FILE NAME]_outputs. In that folder you will have:
    
    1) Plots similar to figure 2, 3, 4 in Bresnahan and Reiss
    2) csv's of the data used to make 1)
    3) data for tables in like in tables 2-6 of Bresnahan  
    
    I will add output for tables 7-9 after work tomorrow (today, I suppose). If you want to save results of different specifications, just rename/move the folder the results you want to save are in. Once we have decided on what specifications/industries to use, I can provide code to put all the final results together into amalgamated tables and plots.

--- making tables and figures_IN PROGRESS.R
+++ making tables and figures_IN PROGRESS.R
@@ -8,18 +8,9 @@ library(ggthemes) #only for some default plot themes
 library(sf) #only in event rds file is sf
 
 #obviously, if your name is not Lance Taylor you should update this to some other folder
-basePath <- 'G:\\My Drive\\phd\\2nd year\\term 2\\io\\hw 2\\'
+basePath <- 'G:\\My Drive\\phd\\2nd year\\term 2\\io\\hw 2\\inputs\\'
 
-dfOg <- basePath%>%
-  #assuming the input file name stays constant, otherwise you'll want to update this
-  paste0('inputs\\Markets_Data.rds')%>%
-  #chances are whoever finalized the data will save it as a csv, so read_csv obvs
-  read_rds%>%
-  as_tibble%>%
-  #mutate is only here since I want to try different vars I AM NOT SAYING THIS IS WHAT WE USE
-  mutate(popDensity = Pop2021 / LANDAREA)%>%
-  #rename is only here because who the hell puts a space at the end of a column name
-  rename_with(~str_squish(.))
+industryFile <- 'Markets_Data.rds'
 
 #whatever variable is used as the desired count of establishments 
 nVar <- 'numberOfEstablishments_1kmBuffer'
@@ -32,9 +23,31 @@ zVars <- c("Single_detached_count", "Avg_fam_size", "Avg_children_if_children")
 #demand and cost shifting vars 
 wVars <- 'popDensity'
 
+#this sets what the upper bond of establishments considered in i.e. original bresnahan and reiss paper had everything
+#at 5 and beyond pooled as 5+ (or less for some industries)
+poolOver <- 2
+
+fileName <- industryFile%>%
+  str_remove('\\..+$')
+
+outFolder <- paste0(basePath, fileName, '_outputs\\')
+
+if(!dir.exists(outFolder)) dir.create(outFolder)
+
+dfOg <- basePath%>%
+  #assuming the input file name stays constant, otherwise you'll want to update this
+  paste0(industryFile)%>%
+  #chances are whoever finalized the data will save it as a csv, so read_csv obvs
+  read_rds%>%
+  as_tibble%>%
+  #mutate is only here since I want to try different vars I AM NOT SAYING THIS IS WHAT WE USE
+  mutate(popDensity = Pop2021 / LANDAREA)%>%
+  #rename is only here because who the hell puts a space at the end of a column name
+  rename_with(~str_squish(.))
+
 #we don't have enough obs with more than 2 vets, I think that is causing it to fail
 df <- dfOg%>%
-  mutate(!!nVar := ifelse(get(nVar) > 2, 2, get(nVar)),
+  mutate(!!nVar := ifelse(get(nVar) > poolOver, poolOver, get(nVar)),
          #also centering and rescaling all non market size vars
          across(all_of(c(wVars, zVars)), ~((.)-mean(.))/sd(.)),
          #any market size var is in terms of deviations from mean 2021 population
@@ -57,9 +70,11 @@ profitFun <- function(w, y, z, lambda, beta, gamma_L, alpha, gamma_n, n){
   
   V_n <- cbind(w, z) %*% beta + alpha[1] 
   
-  if(n > 1) V_n <- V_n - sum(alpha[2:n])
+  if(length(alpha) > 1 & n > 1) V_n <- V_n - sum(alpha[2:min(c(n, length(alpha)))])
   
-  F_n <- w %*% gamma_L + sum(gamma_n[1:n])
+  F_n <- w %*% gamma_L + gamma_n[1] 
+  
+  if(length(gamma_n) > 1 & n > 1) F_n <- F_n - sum(gamma_n[2:min(c(n,length(gamma_n)))])
   
   profit_n <- S * V_n - F_n
   
@@ -67,8 +82,23 @@ profitFun <- function(w, y, z, lambda, beta, gamma_L, alpha, gamma_n, n){
   
 }
 
+paramsFromList <- function(init){
+  
+  paramLocations <- init%>%
+    map_dbl(~length(.))%>%
+    lag(default = 0)
+  
+  #the key thing is the global assignment here, so within optim() it can be found
+  paramLocations <<- 1:length(init)%>%
+    map(function(i) (1:length(init[[i]])) + sum(paramLocations[1:i]))
+  
+  unlist(init)
+  
+}
+
 #the likelihood is ripped from the paper as well. I had to add in a little bump when zeroes happened since for any
 #initial parameters there would only be probabilities of one or zero which was problematic for taking logs 
+#note this is unconstrained when nFrom = nMax
 logLikelihood_constrained <- function(data, params, nFrom){
   
   W <- data[,wVars]%>%
@@ -132,25 +162,17 @@ logLikelihood_constrained <- function(data, params, nFrom){
 }
 
 #when nMax = allCoefsForN..., same as unconstrained (tested to verify - copy pasted to the bottom)
+#use this for table 4 and 5
 results <- map(1:nMax,
     function(allCoefsForNbeyondThisAreTheSame){
-  
-      #I initially set up initParams as a list, not knowing that optim() refused list inputs. The result is things are a little messier
+      
       initParams <- list(lambda = rep(0.01, length(yVars) - 1),
                          beta = rep(0.01, length(c(wVars, zVars))),
                          gamma_L = rep(0.01, length(wVars)),
                          alpha = rep(0.01, allCoefsForNbeyondThisAreTheSame),
                          gamma_n = rep(0.01, allCoefsForNbeyondThisAreTheSame))
-      
-      #part of the aforementioned messiness
-      paramLocations <- initParams%>%
-        map_dbl(~length(.))%>%
-        lag(default = 0)
-      
-      paramLocations <<- 1:length(initParams)%>%
-        map(function(i) (1:length(initParams[[i]])) + sum(paramLocations[1:i]))
-      
-      params <- unlist(initParams)
+  
+      params <- paramsFromList(initParams)
       
       results <- optim(params, logLikelihood_constrained, data = df, nFrom = allCoefsForNbeyondThisAreTheSame, method = "Nelder-Mead", hessian = TRUE,
                        control = list(abstol = .000000025,
@@ -165,9 +187,24 @@ results <- map(1:nMax,
 resultsUnconstrained <- results[[nMax]]
 
 
-#table 5 (entry threshold estimates and likelihood ratio tests for threshold proportionality)
-#figure 4 S_nMax/S_n vs N plot
-#table 6 likelihood ratio tests for equal fixed costs 
+#all firms in market have same fixed cost, i.e. table 6, aka F_n = gamma_1 + gamma_l W (no sum gamma_n)
+initParams_sameCosts <- list(lambda = rep(0.01, length(yVars) - 1),
+                             beta = rep(0.01, length(c(wVars, zVars))),
+                             gamma_L = rep(0.01, length(wVars)),
+                             alpha = rep(0.01, nMax),
+                             gamma_n = rep(0.01, 1))
+
+params <- paramsFromList(initParams_sameCosts)
+
+results_no_gamma2orhigher <- optim(params, logLikelihood_constrained, data = df, nFrom = nMax, 
+                                   method = "Nelder-Mead", hessian = TRUE,
+                                   control = list(abstol = .000000025,
+                                                  maxit = 50000,
+                                                  reltol = 1e-11,
+                                                  trace = 4,
+                                                  ndeps = rep(.00001, length(params))))
+
+#need to auto add the degrees freeom to tests
 #table 7 likelihood ratio exclusion tests for the market size regressors 
 #table 8 likelihood ratio exclusion tests for the variable profits regressors 
 #table 9 entry threshold for alternate market definitions 
@@ -187,6 +224,7 @@ figure2 <- figure2Data%>%
 
 figure2
 
+
 #table 2 as a chart
 table2Data <- dfOg%>%
   count(nIncumbents = get(nVar))
@@ -203,10 +241,10 @@ table2Figure
 #figure 3
 figure3Data <- dfOg%>%
   count(popRange = round(get(popVar) / 1000),
-        nIncumbents = factor(get(nVar), 
-                             levels = get(nVar)%>%
-                               unique%>%
-                               sort%>%
+        nIncumbents = ifelse(get(nVar) >= poolOver, '2+', as.character(get(nVar))),
+        nIncumbents = factor(nIncumbents, 
+                             levels = as.character(0:poolOver)%>%
+                               replace(. == poolOver, paste0(poolOver, '+'))%>%
                                rev))
 
 figure3 <- figure3Data%>%
@@ -216,6 +254,7 @@ figure3 <- figure3Data%>%
   scale_fill_economist()+
   labs(y = 'Number of Towns',
        x = 'Town Population Range (000s)',
+       fill = 'number of firms',
        caption = 'each town is at least 1k, bins are increments of 1k')
 
 figure3
@@ -246,7 +285,8 @@ table4Data <- data.frame(term = c(yVars[-1],
                          se = resultsUnconstrained$hessian%>%
                            diag%>%
                            sqrt)%>%
-  rownames_to_column('regTerm')
+  rownames_to_column('regTerm')%>%
+  bind_rows(data.frame(term = 'Log likelihood', value = resultsUnconstrained$value))
 
 #table5a entry threshold estimates 
 table5aData <- (1:nMax)%>%
@@ -275,157 +315,84 @@ table5aData <- (1:nMax)%>%
                den = den$denominator)
     
   })%>%
-  mutate(S_n = num / den)
+  mutate(S_n = num / den,
+         S_n_over_S_nMinusOne = S_n / lag(S_n))
+
+#note since we were minimizing with optim(), output was the minimized negative LL, so LRT has signs switched
+table5bData <- map_dfr(1:(nMax - 1),
+                       function(n) data.frame(LRT = -2*(resultsUnconstrained$value - results[[n]]$value),
+                                              nFrom = n))
+
+#not making plot yet since with only vets we had just one firm and one relevant ratio...
+figure4Data <- table5aData%>%
+  mutate(S_nMaxOverS_n = last(S_n)/S_n)
 
 
 
+#table 6 likelihood ratio tests for equal fixed costs 
+table6RegResults <- data.frame(term = c(yVars[-1],
+                                  c(wVars, zVars),
+                                  wVars,
+                                  'V_1 (a_1)',
+                                  'F_1 (g_1)',
+                                  2:nMax%>%
+                                    paste0('V_', .-1, ' - V_', ., ' (a_', ., ')')),
+                         value = results_no_gamma2orhigher$par,
+                         se = results_no_gamma2orhigher$hessian%>%
+                           diag%>%
+                           sqrt)%>%
+  rownames_to_column('regTerm')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#old, but in case needed
-
-logLikelihood <- function(data, params){
-  
-  W <- data[,wVars]%>%
-    data.matrix()
-  
-  Y <- data[,yVars]%>%
-    data.matrix()
-  
-  Z <- data[,zVars]%>%
-    data.matrix()
-  
-  lambda <- matrix(c(1, params[paramLocations[[1]]]), 
-                   nrow = length(paramLocations[[1]]) + 1)
-  
-  beta <- matrix(params[paramLocations[[2]]], 
-                 nrow = length(paramLocations[[2]]))
-  
-  gamma_L <- matrix(params[paramLocations[[3]]],
-                    nrow = length(paramLocations[[3]]))
-  
-  alpha <- matrix(params[paramLocations[[4]]], 
-                  nrow = length(params[paramLocations[[4]]]))
-  
-  gamma_n <- matrix(params[paramLocations[[5]]], 
-                    nrow = length(params[paramLocations[[5]]]))
-  
-  pi_n <- map(1:nMax, ~profitFun(W, Y, Z, 
-                                 lambda,#matrix(rep(.001, 2), nrow = 2),
-                                 beta,#matrix(rep(.001, 4), nrow = 4),
-                                 gamma_L,#matrix(rep(.001, 1), nrow = 1),
-                                 alpha,#matrix(rep(.001, 5), nrow = 5),
-                                 gamma_n,#matrix(rep(.001, 5), nrow = 5),
-                                 .))
-  
-  ll <- cbind(1 - pnorm(pi_n[[1]], lower.tail = FALSE),
-              map(1:(nMax - 1), ~ (pnorm(pi_n[[.]], lower.tail = FALSE) - pnorm(pi_n[[. + 1]], lower.tail = FALSE))[,1])%>%
-                simplify2array(),
-              pnorm(pi_n[[nMax]], lower.tail = FALSE))
-  
-  #unsure how Kosher this is, but I was having issues with starting values otherwise
-  for(i in 1:nrow(ll)){
+table6Data <- (1:nMax)%>%
+  map_dfr(function(n){
     
-    l <- ll[i,]
+    num <- table3Data%>%
+      inner_join(table6RegResults%>%
+                   filter(str_detect(regTerm, 'gamma'))%>%
+                   mutate(wVar = TRUE),
+                 by = c('variable' = 'term'))%>%
+      summarise(numerator = sum(value * mean) + table6RegResults$value[table6RegResults$regTerm == 'gamma_n'])
     
-    check <- which(l < 0.00001)
+    den <- table3Data%>%
+      inner_join(table6RegResults%>%
+                   filter(str_detect(regTerm, 'beta'))%>%
+                   mutate(xVar = TRUE),
+                 by = c('variable' = 'term'))%>%
+      summarise(denominator = sum(value * mean) + table6RegResults$value[table6RegResults$regTerm == 'alpha1'])
     
-    l[check] <- 0.0001
+    if(n > 1) den <- den - sum(table6RegResults$value[str_detect(table6RegResults$regTerm, 'alpha[^1][0-9]*$')])
     
-    l <- l / sum(l)
+    data.frame(N = n,
+               num = num$numerator,
+               den = den$denominator)
     
-    ll[i,] <- l
-    
-  }
-  
-  ll <- rowSums(log(ll) * nEstablishmentsDummyMatrix)
-  
-  out <- -sum(ll)
-  
-  out
-  
-}
-
-
-
-allCoefsForNbeyondThisAreTheSame <- 1
-initParams <- list(lambda = rep(0.01, length(yVars) - 1),
-                   beta = rep(0.01, length(c(wVars, zVars))),
-                   gamma_L = rep(0.01, length(wVars)),
-                   alpha = rep(0.01, allCoefsForNbeyondThisAreTheSame),
-                   gamma_n = rep(0.01, allCoefsForNbeyondThisAreTheSame))
-
-#part of the aforementioned messiness
-paramLocations <- initParams%>%
-  map_dbl(~length(.))%>%
-  lag(default = 0)
-
-paramLocations <- 1:length(initParams)%>%
-  map(function(i) (1:length(initParams[[i]])) + sum(paramLocations[1:i]))
-
-params <- unlist(initParams)
-
-
-
-results <- optim(params, logLikelihood_constrained, data = df, nFrom = allCoefsForNbeyondThisAreTheSame, method = "Nelder-Mead", hessian = TRUE,
-                 control = list(abstol = .000000025,
-                                maxit = 50000,
-                                reltol = 1e-11,
-                                trace = 4,
-                                ndeps = rep(.00001, length(params))))
-results
+  })%>%
+  mutate(S_n = num / den,
+         S_n_over_S_nMinusOne = S_n / lag(S_n),
+         Srat = paste0('S_', N, '/S_', lag(N)))%>%
+  filter(N > 1)%>%
+  select(-c(N:S_n))%>%
+  pivot_wider(names_from = Srat, values_from = S_n_over_S_nMinusOne)%>%
+  mutate(LikelihoodValue = results_no_gamma2orhigher$value,
+         LRT = -2*(resultsUnconstrained$value - results_no_gamma2orhigher$value),
+         degreesFreedom = nMax - 1)%>%
+  relocate(matches('^S'), .after = degreesFreedom)
 
 
 
 
-#I initially set up initParams as a list, not knowing that optim() refused list inputs. The result is things are a little messier
-initParams <- list(lambda = rep(0.01, length(yVars) - 1),
-                   beta = rep(0.01, length(c(wVars, zVars))),
-                   gamma_L = rep(0.01, length(wVars)),
-                   alpha = rep(0.01, nMax),
-                   gamma_n = rep(0.01, nMax))
 
-#part of the aforementioned messiness
-paramLocations <- initParams%>%
-  map_dbl(~length(.))%>%
-  lag(default = 0)
+ls()%>%
+  str_subset('igure.{0,1}$')%>%
+  walk(~ggsave(paste0(outFolder, ., '.png'), 
+               plot = get(.)))
 
-paramLocations <- 1:length(initParams)%>%
-  map(function(i) (1:length(initParams[[i]])) + sum(paramLocations[1:i]))
+ls()%>%
+  str_subset('[dD]ata')%>%
+  walk(~write_csv(get(.), paste0(outFolder, ., '.csv')))
 
-params <- unlist(initParams)
 
-#I can only get it to run when we cap n at 2, and I use Nelder-Mead
-#NOTE: in original paper, I do not know how they handled the W variables in their table
-#W can show up in V or F, and so should have a gamma_l and beta coef 
-#but their W variable LANDV is only reported with a gamma_l
-results <- optim(params, logLikelihood, data = df, method = "Nelder-Mead", hessian = TRUE,
-                 control = list(abstol = .000000025,
-                                maxit = 50000,
-                                reltol = 1e-11,
-                                trace = 4,
-                                ndeps = rep(.00001, length(params))))
 
-results_unconstrained <- results
 
 
 
